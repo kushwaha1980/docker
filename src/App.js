c6c057202b878522216d6996975a838c52a435b5
kumard31 <ee_deepakkumar@rediffmail.com>
2019-08-01 08:02:18 -0400

    app page modification

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-	  This is first AWS EB project!!!!
+	  Coming via feature branch!!!!
         </p>
         <a
           className="App-link"
